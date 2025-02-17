87821e69eb400addfc83882f8037ad356e5d5cbe
srnabab <yluo0942@gmail.com>
2025-02-17 11:23:29 +0800

    test2

--- bot.py
+++ bot.py
@@ -1,19 +1,13 @@
 import discord
-from discord.commands import slash_command
 import os
 
 TOKEN = os.environ.get('TOKEN')
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
 intents = discord.Intents.default()
-bot = discord.Bot(intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'机器人已登录，用户名为 {bot.user}')
-
-@slash_command(guild_ids=[1320038130551881759]) # 指定服务器 ID 列表
-async def ping(ctx):
-    """回复 Pong!"""
-    await ctx.respond("Pong!")
-
-bot.run(TOKEN)
+client = MyClient(intents=intents)
+client.run(TOKEN)
