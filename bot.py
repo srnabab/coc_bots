ef9b75b4abed5af94f3d6378cebe5ecf8a14443e
srnabab <yluo0942@gmail.com>
2025-02-17 11:31:39 +0800

    http port process

--- bot.py
+++ bot.py
@@ -1,5 +1,19 @@
 import discord
 import os
+import http.server
+import socketserver
+from threading import Thread
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Bot is running!")
+
+def run_http_server():
+    with socketserver.TCPServer(("0.0.0.0", 8080), MyHandler) as httpd:
+        httpd.serve_forever()
 
 TOKEN = os.environ.get('TOKEN')
 
@@ -7,7 +21,12 @@ class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
-intents = discord.Intents.default()
 
-client = MyClient(intents=intents)
-client.run(TOKEN)
+if __name__ == "__main__":
+    http_thread = Thread(target=run_http_server)
+    http_thread.start()
+    
+    intents = discord.Intents.default()
+    client = MyClient(intents=intents)
+    client.run(TOKEN)
+    
